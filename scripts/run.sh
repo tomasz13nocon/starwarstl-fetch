f2955a8eef3be80cc981d998b8032c79353492a6
tomasz13nocon <tomasz13nocon@gmail.com>
2023-03-28 16:11:18 +0200

    cleanup run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 [ -z $FETCH_DIR ] && echo "FETCH_DIR env var not set. Aborting." && exit 1
-[ -z $MAIL_BOT_PASS ] && echo "MAIL_BOT_PASS env var not set. Aborting." && exit 1
+[ -z $MAILBOT_SERVER ] && echo "MAILBOT_SERVER env var not set. Aborting." && exit 1
+[ -z $MAILBOT_FROM ] && echo "MAILBOT_FROM env var not set. Aborting." && exit 1
+[ -z $MAILBOT_RCPT ] && echo "MAILBOT_RCPT env var not set. Aborting." && exit 1
+[ -z $MAILBOT_PASS ] && echo "MAILBOT_PASS env var not set. Aborting." && exit 1
 
 cd $FETCH_DIR
 rm -rf .cache
@@ -10,10 +13,14 @@ timestamp=$(date '+%Y-%m-%d_%H:%M:%S')
 log_file="logs/$timestamp"
 log_file_err="${log_file}.err"
 
-node src/fetch_timeline.js -c > "$log_file" 2> "$log_file_err"
+node src/index.js -c > "$log_file" 2> "$log_file_err"
 
 if [ -s "$log_file_err" ]; then
-  # TODO split
-  curl --url "smtps://smtp.gmail.com:465" --ssl-reqd --mail-from "starwarstl13@gmail.com" --mail-rcpt "mail@starwarstl.com" --user "starwarstl13@gmail.com:$MAIL_BOT_PASS" -T <(echo -e "Subject: Errors during fetching timeline\n" | cat - "$log_file_err")
+  curl --url "$MAILBOT_SERVER" \
+       --ssl-reqd \
+       --mail-from "$MAILBOT_FROM" \
+       --mail-rcpt "$MAILBOT_RCPT" \
+       --user "$MAILBOT_FROM:$MAIL_BOT_PASS" \
+       -T <(echo -e "Subject: Errors during fetching timeline\n" | cat - "$log_file_err")
 fi
 
